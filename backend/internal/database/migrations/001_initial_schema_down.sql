ece82a8ee7f1cad12be4b3549cf663ae46dabdac
Thanikan <sthanikan2000@gmail.com>
2026-01-24 00:33:07 +0530

    feat: Add initial database schema and rollback script for NSW workflow management system

---
+++ backend/internal/database/migrations/001_initial_schema_down.sql
@@ -0,0 +1,12 @@
+-- Migration: 001_initial_schema_down.sql
+-- Description: Rollback initial database schema
+-- Created: 2026-01-23
+
+-- Drop tables in reverse order of creation (respecting foreign key dependencies)
+DROP TABLE IF EXISTS tasks CASCADE;
+DROP TABLE IF EXISTS consignments CASCADE;
+DROP TABLE IF EXISTS workflow_template_maps CASCADE;
+DROP TABLE IF EXISTS workflow_templates CASCADE;
+DROP TABLE IF EXISTS hs_codes CASCADE;
+
+-- Note: We don't drop the uuid-ossp extension as it might be used by other schemas
