532a8fd2ff76a3e51ee9c9728d6822eafd0864b4
Your Name <regina.xu1@gmail.com>
2026-01-29 10:21:33 +0530

    Address PR comments

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -43,11 +43,6 @@ func main() {
 		"max_age", cfg.CORS.MaxAge,
 	)
 
-	var (
-		Version = "dev"
-		Commit  = "none"
-	)
-
 	slog.Info("server configuration",
 		"port", cfg.Server.Port,
 		"version", Version,
