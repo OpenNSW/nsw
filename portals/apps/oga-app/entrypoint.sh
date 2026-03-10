0c8455c5fdafad5239a1cd13b77a9edcce35c3b6
Isala Piyarisi <mail@isala.me>
2026-03-10 19:33:00 +0530

    Add Dockerfiles and workload.yaml descriptors for OpenChoreo source builds

---
+++ portals/apps/oga-app/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+cat > /usr/share/nginx/html/config.js << EOF
+window.__APP_CONFIG__ = {
+  VITE_API_BASE_URL: '${VITE_API_BASE_URL:-}',
+  VITE_IDP_CLIENT_ID: '${VITE_IDP_CLIENT_ID:-}',
+  VITE_IDP_BASE_URL: '${VITE_IDP_BASE_URL:-}',
+  VITE_IDP_PLATFORM: '${VITE_IDP_PLATFORM:-}',
+  VITE_IDP_SCOPES: '${VITE_IDP_SCOPES:-}',
+  VITE_APP_URL: '${VITE_APP_URL:-}'
+};
+EOF
+exec nginx -g 'daemon off;'
