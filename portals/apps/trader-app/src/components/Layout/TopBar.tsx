a1c899c5a69b9b3eb6d043fec1432c4ebc488bb2
Thanikan <sthanikan2000@gmail.com>
2026-04-09 19:37:46 +0530

    feat: remove UserOnlyTopBar component and update UnauthorizedScreen layout

--- portals/apps/trader-app/src/components/Layout/TopBar.tsx
+++ portals/apps/trader-app/src/components/Layout/TopBar.tsx
@@ -32,16 +32,6 @@ function TopBarUserActions({ onSignOut, withDivider = true }: { onSignOut: () =>
   )
 }
 
-export function UserOnlyTopBar() {
-  const handleSignOut = useSignOutHandler()
-
-  return (
-    <TopBarShell>
-      <TopBarUserActions onSignOut={handleSignOut} withDivider={false}/>
-    </TopBarShell>
-  )
-}
-
 export function TopBar() {
   const handleSignOut = useSignOutHandler()
 
@@ -49,12 +39,12 @@ export function TopBar() {
     <TopBarShell>
         <RoleSwitcher />
         {/* Notifications */}
+        {/* TODO: Show real notifications and link to a notifications page */}
         <button
           className="relative p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition-colors">
           <BellIcon className="w-5 h-5"/>
           <span className="absolute top-1.5 right-1.5 w-2 h-2 bg-red-500 rounded-full"></span>
         </button>
-
         <TopBarUserActions onSignOut={handleSignOut}/>
     </TopBarShell>
   )
